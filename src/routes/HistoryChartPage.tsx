e1de2750091a6f54c61955e29a109650eeb9abc2
sharp <dylan.mark.sharp@gmail.com>
2024-03-31 22:15:29 +0200

    Remove redundant data call.

--- src/routes/HistoryChartPage.tsx
+++ src/routes/HistoryChartPage.tsx
@@ -1,19 +1,10 @@
-import {useParams} from "react-router-dom";
-import useWeatherDataHistory from "../hooks/use-weather-data-history.ts";
 import {Card} from "konsta/react";
-import LoadingSpinner from "../components/LoadingSpinner.tsx";
 import Chart from "../components/Chart.tsx";
 
 const HistoryChartPage = () => {
-    const {locationId} = useParams();
-    const {serializedData, isLoading, isError} = useWeatherDataHistory(locationId);
     return (
         <Card>
-            {isLoading ? (
-                <LoadingSpinner/>
-            ) : (
-                <Chart/>
-            )}
+            <Chart/>
         </Card>
     )
 }
